ecb8363a59eb9255dd5ea92e74ba67111d94ff16
pr0crustes <dark.pr0crustes@gmail.com>
2019-07-18 12:55:30 -0300

    Fixed zero bug.

--- pgrades/index.js
+++ pgrades/index.js
@@ -10,10 +10,7 @@ function InputAsNumber(id) {
     var text = document.getElementById(id).value.trim();
     text.replace(",", ".");
     var as_number = parseFloat(text);
-    if (as_number && as_number !== NaN) {
-        return as_number;
-    }
-    return null;
+    return ValidNumbers(as_number) ? as_number : NaN;
 }
 
 function SetInput(id, v) {
@@ -45,31 +42,40 @@ function GetMF() {
     return InputAsNumber("input_mf");
 }
 
+function ValidNumbers() {
+    for (var i = 0; i < arguments.length; i++) {
+        if (isNaN(arguments[i])) {
+            return false;
+        }
+    }
+    return true;
+}
+
 function Calculate() {
     let p1 = GetP1();
     let p2 = GetP2();
     let p3 = GetP3();
     let mf = GetMF();
 
-    if (p1 && p2 && p3) {
+    if (ValidNumbers(p1, p2, p3)) {
         let total = ((4 * p1) + (4 * p2) + (2 * p3)) / 10;
         SetInput("input_mf", total);
         return true;
     }
 
-    if (p1 && p2 && mf) {
+    if (ValidNumbers(p1, p2, mf)) {
         let needed_p3 = (10 * mf - (4 * p1) - (4 * p2)) / 2;
         SetInput("input_p3", needed_p3);
         return true;
     }
 
-    if (p2 && p3 && mf) {
+    if (ValidNumbers(p2, p3, mf)) {
         let needed_p1 = (10 * mf - (4 * p2) - (2 * p3)) / 4;
         SetInput("input_p1", needed_p1);
         return true;
     }
 
-    if (p1 && p3 && mf) {
+    if (ValidNumbers(p1, p3, mf)) {
         let needed_p2 = (10 * mf - (4 * p1) - (2 * p3)) / 4;
         SetInput("input_p2", needed_p2);
         return true;
